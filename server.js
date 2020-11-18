52c1d6589e351e3024dfc0b50b0d1241a862f9d5
Opilane TTHK <opilane@tthk.ee>
2020-11-18 11:12:41 +0200

    m

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+//const ejs = require("ejs");
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*mongoose.connect('mongodb:/localhost:27017/secretDB',
+{useNewUrlParser: true, useUnifiedTopology: true});*/
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/register', (req, res) =>{
+    res.render('register');
+})
+
+app.listen(3000, () => {
+    console.log("connected on port 3000");
+})
